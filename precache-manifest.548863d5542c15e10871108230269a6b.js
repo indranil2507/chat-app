0615ff9a6a70e14b3fd2b53ffed584f38fc3b6a4
indranil2507 <iindranilsaha1@gmail.com>
2020-08-15 04:45:25 +0530

    Updates

---
+++ precache-manifest.548863d5542c15e10871108230269a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3666f8b76dd368de90ae733e409bc3",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "8361aa3684026aa8bd46",
+    "url": "/chat-app/static/css/main.9411f3af.chunk.css"
+  },
+  {
+    "revision": "2184eaa5139d3da70017",
+    "url": "/chat-app/static/js/2.95d79793.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chat-app/static/js/2.95d79793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8361aa3684026aa8bd46",
+    "url": "/chat-app/static/js/main.6e54591b.chunk.js"
+  },
+  {
+    "revision": "844d941a2013a08fe2e0",
+    "url": "/chat-app/static/js/runtime-main.bdb3b2b2.js"
+  }
+]);
